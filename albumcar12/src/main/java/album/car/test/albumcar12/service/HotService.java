24405127ea0c83b0a4280535fe474facb28abd55
KaueVitsrki <kauefernandocunha@gmail.com>
2024-04-26 15:23:42 -0300

    hot implementation. removed: findHotById method in hotRepository

--- albumcar12/src/main/java/album/car/test/albumcar12/service/HotService.java
+++ albumcar12/src/main/java/album/car/test/albumcar12/service/HotService.java
@@ -1,6 +1,5 @@
 package album.car.test.albumcar12.service;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
@@ -14,7 +13,6 @@ import album.car.test.albumcar12.model.AlbumModel;
 import album.car.test.albumcar12.model.HotModel;
 import album.car.test.albumcar12.model.UserModel;
 import album.car.test.albumcar12.repository.AlbumRepository;
-import album.car.test.albumcar12.repository.HotRepository;
 import album.car.test.albumcar12.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
@@ -27,8 +25,6 @@ public class HotService {
     private AlbumRepository albumRepository;
     @Autowired
     private ModelMapper modelMapper;
-    @Autowired
-    private HotRepository hotRepository;
 
     @Transactional
     public HotDto createHot(UUID idUser, UUID idAlbum){
@@ -40,14 +36,14 @@ public class HotService {
         }
 
         UserModel user = userRepository.findUserById(idUser);
-        List<HotModel> userHot = user.getHot();
-        boolean giveHot = userHot.stream().anyMatch(hot -> hot.getUser().getId().equals(user.getId()));
+        AlbumModel albumModel = albumRepository.findAlbumById(idAlbum);
+        boolean userAlreadyRatedAlbum  = user.getHot().stream().anyMatch(hot -> hot.getAlbum().getId().equals(albumModel.getId()));
+        boolean albumAlreadyRatedByUser  = albumModel.getHot().stream().anyMatch(hot -> hot.getUser().getId().equals(user.getId()));
 
-        if(giveHot){
+        if(userAlreadyRatedAlbum || albumAlreadyRatedByUser){
             throw new UserGiveHotException("Não foi possível avaliar o album, você já o avaliou");
         }
 
-        AlbumModel albumModel = albumRepository.findAlbumById(idAlbum);
         HotModel hotModel = new HotModel();
         hotModel.setAlbum(albumModel);
         hotModel.setUser(user);
@@ -63,30 +59,28 @@ public class HotService {
     @Transactional
     public void deleteHot(UUID idUser, UUID idAlbum, UUID idHot){
         if(!userRepository.existsById(idUser)){
-            throw new EntityNotFoundException("Não foi possível criar o diary! O usuário não existe");
+            throw new EntityNotFoundException("Não foi possível remover o hot! O usuário não existe");
         }
         if(!albumRepository.existsById(idAlbum)){
-            throw new EntityNotFoundException("Não foi possível criar o diary! O album não existe");
+            throw new EntityNotFoundException("Não foi possível remover o hot! O album não existe");
         }
 
         UserModel user = userRepository.findUserById(idUser);
-        List<HotModel> userHot = user.getHot();
-        boolean giveHot = userHot.stream().anyMatch(hot -> hot.getUser().getId().equals(user.getId()));
+        AlbumModel albumModel = albumRepository.findAlbumById(idAlbum);
+        boolean userAlreadyRatedAlbum  = user.getHot().stream().anyMatch(hot -> hot.getAlbum().getId().equals(albumModel.getId()));
+        boolean albumAlreadyRatedByUser  = albumModel.getHot().stream().anyMatch(hot -> hot.getUser().getId().equals(user.getId()));
 
-        if(!giveHot){
-            throw new UserGiveHotException("Não foi possível retirar o hot, você não avaliou o album");
+        if(!userAlreadyRatedAlbum || !albumAlreadyRatedByUser){
+            throw new UserGiveHotException("Não foi possível remover o hot, você não o avaliou");
         }
 
-        HotModel hotModel = hotRepository.findHotById(idHot);
-        HotModel removeHot = userHot.stream()
-        .filter(hot -> hot.getId().equals(hotModel.getId()))
+        HotModel removeHot = user.getHot().stream()
+        .filter(hot -> hot.getId().equals(idHot))
         .findFirst()
         .orElseThrow(() -> new NoSuchElementException("Não foi possível encontrar o hot"));
-        AlbumModel albumModel = albumRepository.findAlbumById(idAlbum);
-        List<HotModel> hotAlbum = albumModel.getHot();
 
-        userHot.remove(removeHot);
-        hotAlbum.remove(removeHot);
+        user.getHot().remove(removeHot);
+        albumModel.getHot().remove(removeHot);
         userRepository.save(user);
         albumRepository.save(albumModel);
     }
